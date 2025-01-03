cefb6008a198f960127a2ef06a596cd38619ab7a
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2025-01-03 20:45:34 -0300

    wip: loading song and waveform 2d overlay

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,26 @@
+const W = 400
+const H = 400
+let song
+let waveformCanvas
+
+function preload() {
+  song = loadSound('./audio/griselda type beat.mp3')
+}
+
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(W, H, WEBGL)
+  song.play()
+  waveformCanvas = createGraphics(W, H)
+  waveformCanvas.background(0, 128, 255, 64)
 }
 
 function draw() {
-    background(220);
+  orbitControl()
+  background(0)
+  box(50)
+
+  push()
+  translate(-W / 2, -H / 2)
+  image(waveformCanvas, 0, 0)
+  pop()
 }
