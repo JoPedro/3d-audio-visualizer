6b1892601c324f9a21226399444287ab26d231ae
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2025-01-05 00:27:48 -0300

    feat: waveform and spinning cube

--- public/sketch.js
+++ public/sketch.js
@@ -1,26 +1,53 @@
 const W = 400
 const H = 400
+
 let song
 let waveformCanvas
+let fft
+let waveform
 
 function preload() {
   song = loadSound('./audio/griselda type beat.mp3')
 }
 
+function mouseClicked() {
+  if (!song.isPlaying()) {
+    song.play()
+  }
+}
+
 function setup() {
   createCanvas(W, H, WEBGL)
-  song.play()
   waveformCanvas = createGraphics(W, H)
-  waveformCanvas.background(0, 128, 255, 64)
+  fft = new p5.FFT()
 }
 
 function draw() {
-  orbitControl()
-  background(0)
-  box(50)
+  // orbitControl()
+  background('#232627')
+  waveform = fft.waveform();
+  noFill()
 
   push()
-  translate(-W / 2, -H / 2)
+  stroke('#11d116')
+  rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.02);
+  box(50)
+  pop()
+
+  push()
+  stroke(255)
+  translate(-width / 2, -height / 2)
   image(waveformCanvas, 0, 0)
+
+  beginShape()
+  for (let i = 0; i < waveform.length; i++) {
+    // waveform values are -1 to 1, mapped to width and height
+    let x = map(i, 0, waveform.length, 0, width)
+    let y = map(waveform[i], -1, 1, 0, height / 2)
+    vertex(x, y + height / 4)
+  }
+  endShape()
+
   pop()
 }
