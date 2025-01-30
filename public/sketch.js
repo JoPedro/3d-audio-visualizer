a6b16677995e091753329d6668bb988f459a9fd4
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2025-01-30 17:32:45 -0300

    wip: triangle strip mesh and rescaling

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,9 @@
-const W = 400
-const H = 400
+const WIDTH = 1280
+const HEIGHT = 720
 const PLAY = 'play'
 const PAUSE = 'pause'
 const STOP = 'stop'
+const SCALE = 128
 
 let song
 let waveformCanvas
@@ -10,6 +11,8 @@ let fft
 let waveform
 let playToggle
 let stopBtn
+let rows
+let cols
 
 function preload() {
   song = loadSound('./audio/griselda type beat.mp3')
@@ -24,8 +27,11 @@ function toggleLabel() {
 }
 
 function setup() {
-  createCanvas(W, H, WEBGL)
-  waveformCanvas = createGraphics(W, H)
+  createCanvas(WIDTH, HEIGHT, WEBGL)
+  cols = WIDTH / SCALE;
+  rows = HEIGHT / SCALE;
+
+  waveformCanvas = createGraphics(WIDTH, HEIGHT)
   fft = new p5.FFT()
 
   playToggle = createButton(PLAY)
@@ -45,21 +51,37 @@ function setup() {
 }
 
 function draw() {
-  // orbitControl()
   background('#232627')
+  orbitControl()
   waveform = fft.waveform()
   noFill()
 
-  push()
+  translate(-width / 2, -height / 2)
   stroke(255)
-  rotateX(frameCount * 0.02)
-  rotateY(frameCount * 0.02)
-  box(50)
-  pop()
 
   push()
+  fill('#3d3d3d')
+  translate(0, 128, -720)
+  rotateX(PI / 3)
+  for (let y = 0; y < rows; y++) {
+    beginShape(TRIANGLE_STRIP)
+    for (let x = 0; x < cols; x++) {
+      if (y % 2 === 0) {
+        vertex(x * SCALE, (y + 1) * SCALE)
+        vertex(x * SCALE, y * SCALE)
+      }
+      else {
+        vertex(x * SCALE, y * SCALE)
+        vertex(x * SCALE, (y + 1) * SCALE)
+      }
+    }
+    endShape()
+  }
+  pop()
+
+  translate(0, 0, 64)
+
   stroke('#11d116')
-  translate(-width / 2, -height / 2)
   image(waveformCanvas, 0, 0)
 
   beginShape()
@@ -67,8 +89,8 @@ function draw() {
     // waveform values are -1 to 1, mapped to width and height
     let x = map(i, 0, waveform.length, 0, width)
     let y = map(waveform[i], -1, 1, 0, height / 2)
+    // flatten the max amplitude to fit half the height
     vertex(x, y + height / 4)
   }
   endShape()
-  pop()
 }
